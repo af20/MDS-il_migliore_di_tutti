10f554b8aa147ed1c2478fada757d1e7bb3b7216
AF20 <ferrari20@student.unisi.it>
2021-12-29 15:15:06 +0100

    miglioramento in populate_tables

--- populate_tables.py
+++ populate_tables.py
@@ -6,7 +6,7 @@ def lib_read_csv_file_bgg():
     data = pd.read_csv(file_name, sep=',',na_values=pd.NA)
     df = pd.DataFrame(data, columns = ['game','title','rating'])
     df = df.rename(columns= {'game': 'id', 'rating': 'vote'})
-    df.dropna(how='any', inplace=True)
+    df.dropna(how='any', inplace=True)  # escludiamo le reviews dove 'vote' è vuoto
     v_columns = ['id', 'title', 'vote']
     df = df.reset_index()[v_columns]
     return df
@@ -14,12 +14,15 @@ def lib_read_csv_file_bgg():
 DF = lib_read_csv_file_bgg()
 
 df_unique = DF[['id','title']].drop_duplicates()
-if True:
+db_game_records = engine.execute("SELECT COUNT(*) FROM game").fetchall()[0][0]
+
+if db_game_records == 0: # 23,810
   df_unique.to_sql('game', engine, if_exists='append', index=False, chunksize=10000)
 
 df_review = DF[['id', 'vote']]
 df_review = df_review.rename(columns= {'id': 'id_game'})
 df_review['id'] = df_review.index.values
 
-if True:
+db_review_records = engine.execute("SELECT COUNT(*) FROM review").fetchall()[0][0]
+if db_review_records == 0: # 706,813
   df_review.to_sql('review', engine, if_exists='append', index=False, chunksize=10000)
